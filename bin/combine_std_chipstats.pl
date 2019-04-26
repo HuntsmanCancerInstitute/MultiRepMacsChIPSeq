3e648205f1f0fead4ddc9935c822612413324e6d
Timothy Parnell <parnell.tj@gmail.com>
2019-04-26 14:29:13 -0600

    update to process given files and discard empty columns
    No longer dependent on pysano job directories, although it still
    handles those.

--- bin/combine_std_chipstats.pl
+++ bin/combine_std_chipstats.pl
@@ -14,6 +14,7 @@
 
 use strict;
 use IO::File;
+use List::Util qw(sum0);
 
 unless (@ARGV) {
 	print <<END;
@@ -23,22 +24,58 @@ A script to combine
     - bam2wig empirical shift determination 
     - Macs2 predicted shift determination
 
-It parses this information from stderr.txt and stdout.txt files from Pysano job
+It parses this information from standard output and/or error text files from these 
+programs. It will also parse the stderr.txt and stdout.txt files from Pysano job
 directories. It will write out a single tab-delimited with the numbers. Sample 
-names are the directory names.
+names are the given input file names or directory names.
 
-Usage: $0 <outputfile> 1234X1/ 1234X2/ 1234X3/ ...
+Usage: 
+    
+    Pysano directories:
+        combine_std_chipstats.pl <outputfile> 1234X1/ 1234X2/ ...
+    
+    Text files:
+        combine_std_chipstats.pl <outputfile> file1.txt file2.txt ...
+    
 END
 	exit;
 }
 
+# output file
 my $outfile = shift @ARGV;
+if ($outfile !~ /\.txt$/i) {
+	$outfile .= '.txt';
+}
+
+# process input files
 my @output;
 my %sorter;
 my $n = 0;
 while (@ARGV) {
-	my $dir = shift @ARGV;
-	$dir =~ s/\/$//; # strip trailing / if present
+	
+	# look at the given path - it may be a directory or a file
+	my $path = shift @ARGV;
+	$path =~ s/\/$//; # directory may have a trailing slash - remove if present
+	
+	# check for files
+	my @files;
+	if (-d $path) {
+		# definitely a directory
+		my $stdout = "$path/stdout.txt";
+		if (-e $stdout) {
+			# a Pysano standard output file
+			push @files, $stdout;
+		}
+		my $stderr = "$path/stderr.txt";
+		if (-e $stdout) {
+			# a Pysano standard error file
+			push @files, $stderr;
+		}
+	}
+	elsif (-f $path) {
+		# a file
+		push @files, $path;
+	}
 	
 	# possible stdout values
 	my ($total_mapped, $nondup, $optdup, $optduprate, $workcount, $dup, $duprate, 
@@ -50,9 +87,11 @@ while (@ARGV) {
 		$novoNoMapPer, $novoPEmean, $novoPEstdev, $macsFragLength) = 
 		(0, 0, '0.0%', 0, '0.0%', 0, '0.0%', 0, 0.0, 0);
 	
-	# process stdout
-	if (-e "$dir/stdout.txt") {
-		my $fh = IO::File->new("$dir/stdout.txt");
+	
+	# process files
+	foreach my $file (@files) {
+		my $fh = IO::File->new($file) or 
+			die "unable to read $file! $!\n";
 		while (my $line = $fh->getline) {
 			
 			# total
@@ -109,7 +148,7 @@ while (@ARGV) {
 				$optduprate = $1;
 			}
 			
-			# other
+			# bam partial dedup
 			elsif ($line =~ /^  Retained duplicate count:\s+(\d+)\s*$/) {
 				# bam_partial_dedup
 				# oops, there may be a space at the end
@@ -120,36 +159,27 @@ while (@ARGV) {
 				$workcount = $1;
 			}
 			
-			
-			# shift
+			# bam2wig shift value
 			elsif ($line =~ /^  The trimmed mean shift value is (\d+) /) {
-				# bam2wig
 				$trimMeanShift = $1;
 			}
 			elsif ($line =~ /Alignments will be extended by (\d+) bp$/) {
-				# bam2wig
 				$extension = $1;
 			}
-		}
-		$fh->close;
-	}
-	
-	# process stderr
-	if (-e "$dir/stderr.txt") {
-		my $fh = IO::File->new("$dir/stderr.txt");
-		while (my $line = $fh->getline) {
-			if ($line =~  /^#\s+Read Sequences:\s+(\d+)/) {
+			
+			# Novoalign standard error output
+			elsif ($line =~  /^#\s+Read Sequences:\s+(\d+)/) {
 				$novoReads = $1;
 			}
-			if ($line =~  /^#\s+Unique Alignment:\s+(\d+) \( ?(\d+\.\d%)\)$/) {
+			elsif ($line =~  /^#\s+Unique Alignment:\s+(\d+) \( ?(\d+\.\d)%\)$/) {
 				$novoUnique = $1;
 				$novoUniquePer = $2;
 			}
-			elsif ($line =~ /^#\s+Multi Mapped:\s+(\d+) \( ?(\d+\.\d%)\)$/) {
+			elsif ($line =~ /^#\s+Multi Mapped:\s+(\d+) \( ?(\d+\.\d)%\)$/) {
 				$novoMulti = $1;
 				$novoMultiPer = $2;
 			}
-			elsif ($line =~ /^#\s+No Mapping Found:\s+(\d+) \( ?(\d+\.\d%)\)$/) {
+			elsif ($line =~ /^#\s+No Mapping Found:\s+(\d+) \( ?(\d+\.\d)%\)$/) {
 				$novoNoMap = $1;
 				$novoNoMapPer = $2;
 			}
@@ -157,6 +187,8 @@ while (@ARGV) {
 				$novoPEmean = $1;
 				$novoPEstdev = $2;
 			}
+			
+			# macs2 predicted fragment length
 			elsif ($line =~ /^INFO  @ .+: # predicted fragment length is (\d+) bps $/) {
 				$macsFragLength = $1;
 			}
@@ -164,7 +196,10 @@ while (@ARGV) {
 		$fh->close;
 	}
 	
+	# not finding what we're looking for
 	next unless ($novoReads or $total_mapped);
+	
+	# infer some counts
 	if ($nondup and $dup and not $duprate) {
 		# this occurs if we're parsing from bam_umi_dedup
 		$duprate = sprintf("%.4f", $dup / ($dup + $nondup));
@@ -173,43 +208,58 @@ while (@ARGV) {
 		$workcount = $total_mapped;
 	}
 	
+	# clean up path in case we had a file
+	$path =~ s/\.txt$//i;
+	
 	# store
-	push @output, join("\t", $dir, $novoReads, $novoUnique, $novoMulti, $novoNoMap, 
+	push @output, [$path, $novoReads, $novoUnique, $novoMulti, $novoNoMap, 
 		$novoUniquePer, $novoMultiPer, $novoNoMapPer, $novoPEmean, $novoPEstdev, 
 		$total_mapped, $optdup, $optduprate, $workcount, $nondup, $dup, 
-		$duprate, $keepdup, $trimMeanShift, $extension, $macsFragLength);
+		$duprate, $keepdup, $trimMeanShift, $extension, $macsFragLength];
 	
 	# sort order
-	if ($dir =~ /^(\d+)X(\d+)/) {
+	if ($path =~ /^(\d+)X(\d+)/) {
+		# GNomEx identifier
 		$sorter{num}{$1}{$2} = $n;
 	}
 	else {
-		$sorter{char}{$dir} = $n;
+		$sorter{char}{$path} = $n;
 	}
 	
 	# increment counter
 	$n++;
 }
 
-
-
-# write final
-my $fh = IO::File->new($outfile, 'w') or die "can't write to $outfile!\n";
-$fh->printf( "%s\n", join("\t", qw(Sample NovoalignTotalReads NovoalignUniqueMapped NovoalignMultiMap 
+# output headers
+my @headers = qw(Sample NovoalignTotalReads NovoalignUniqueMapped NovoalignMultiMap 
 	NovoalignUnMapped NovoalignUniqueMappedFrac NovoalignMultiMapFrac NovoalignUnMappedFrac 
 	NovoalignInsertMean NovoalignInsertStdDev 
 	TotalMapped OpticalDuplicateCount OpticalRate WorkingCount NonDuplicateCount 
 	DuplicateCount DuplicateRate RetainedDuplicateCount Bam2WigShift Bam2WigExtension 
-	Macs2Extension)));
+	Macs2Extension);
+
+# check for empty columns
+my @columns;
+for my $i (0..$#headers) {
+	my $check = sum0( map {$output[$_]->[$i]} (0..$#output) );
+	push @columns, $i if $check != 0;
+}
+
+
+# write final
+my $fh = IO::File->new($outfile, 'w') or die "can't write to $outfile!\n";
+$fh->printf( "%s\n", join("\t", map { $headers[$_] } @columns) );
 # sort by experiment ID, requestXsample, e.g. 1234X1
 foreach my $i (sort {$a <=> $b} keys %{$sorter{num}}) {
 	foreach my $j (sort {$a <=> $b} keys %{$sorter{num}{$i}}) {
-		$fh->printf("%s\n", $output[ $sorter{num}{$i}{$j} ]);
+		my $row = $output[ $sorter{num}{$i}{$j} ];
+		$fh->printf("%s\n", join("\t", map { $row->[$_] } @columns) );
 	}
 }
 # sort everything else
 foreach my $i (sort {$a cmp $b} keys %{$sorter{char}}) {
-	$fh->printf("%s\n", $output[ $sorter{char}{$i} ]);
+	my $row = $output[ $sorter{char}{$i} ];
+	$fh->printf("%s\n", join("\t", map { $row->[$_] } @columns) );
 }
 $fh->close;
 printf "combined %d samples into $outfile\n", scalar(@output);
