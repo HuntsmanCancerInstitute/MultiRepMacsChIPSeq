7577b32bb91d2763c992e6467ff2fe329fe8b1de
Timothy Parnell <parnell.tj@gmail.com>
2019-12-10 16:05:44 -0700

    fix typo bug

--- bin/multirep_macs2_pipeline.pl
+++ bin/multirep_macs2_pipeline.pl
@@ -1313,10 +1313,10 @@ sub run_efficiency {
 		$fh->close;
 		print "\nWrote combined ChIP efficiency file $combined_eff_out\n";
 	}
-	elsif (scalar @command == 1) {
+	elsif (scalar @commands == 1) {
 		# just one, so rename it for consistency sake
 		my $eff_out = File::Spec->catfile($opts{dir}, $opts{out} . '.chip_efficiency.txt');
-		move($command[0]->[1], $eff_out);
+		move($commands[0]->[1], $eff_out);
 	}
 	
 	update_progress_file('efficiency');
