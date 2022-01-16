f34a2797cc5d619b0bd43173d0cecd3cace851d8
Timothy Parnell <parnell.tj@gmail.com>
2022-01-16 15:02:39 -0700

    update documentation

--- bin/generate_mean_bedGraph.pl
+++ bin/generate_mean_bedGraph.pl
@@ -24,22 +24,25 @@ my $VERSION =3;
 
 my $docs = <<USAGE;
   
-  A script to generate a chromosomal mean coverage bedGraph track 
-  to be used in Macs2 as the global control track when there is 
-  no input for generating a control_lambda chromatin bias track.
-  This uses a bigWig or bedGraph coverage file to calculate a global 
-  mean. Intervals without coverage are not included in the calculation.
-  It will write out a simple bedGraph representing the genome 
-  with the respective mean for each chromosome. 
+A script to generate a chromosomal mean coverage bedGraph track 
+to be used in Macs2 as the global control track when there is 
+no input for generating a control_lambda chromatin bias track.
+This uses a bigWig or bedGraph coverage file to calculate a global 
+mean. Intervals without coverage are not included in the calculation.
+It will write out a simple bedGraph representing the genome 
+with the respective mean for each chromosome. 
 
-  It will write out a bedgraph file in the same directory with the 
-  basename appended with '.global_mean.bdg'. More than one file may 
-  be provided at a time.
-  
-  Version: $VERSION
-   
-  Usage: $0 <file1.bw> ...
-  
+Provide an input file in either bigWig or bedGraph format. 
+
+VERSION: $VERSION
+
+USAGE: $0 -i file.bw -o mean.bdg
+
+OPTIONS:
+    -i --in <file>      The input bigWig or bedGraph file
+    -o --out <file>     The output bedGraph file (input.global_mean.bdg)
+    -h --help           Display help
+        
 USAGE
 
 ### Options
