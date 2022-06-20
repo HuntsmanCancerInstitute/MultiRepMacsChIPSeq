8ca0e556fb5a6d6b5c5d441d14b3c1dcb4265887
Timothy Parnell <parnell.tj@gmail.com>
2022-06-20 14:50:33 -0600

    add UpSetR

--- bin/install_R_packages.R
+++ bin/install_R_packages.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript --no-save --no-restore  
 
-install.packages(c("optparse","RColorBrewer","reshape2","ggplot2","pheatmap"), repos="http://cran.r-project.org")
+install.packages(c("optparse","RColorBrewer","reshape2","ggplot2","pheatmap","UpSetR"), repos="http://cran.r-project.org")
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
