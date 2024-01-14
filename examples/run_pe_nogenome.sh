2af35c6cb9ec90141247e914621939757e9f8491
Timothy Parnell <parnell.tj@gmail.com>
2024-01-14 12:53:26 -0700

    add a test script for no genome
    Provided test bams are so heavily sub-sampled that genome must be supplied to get anything useful. This tests what happens when you don't. Not a real example, but no place else to put it.

---
+++ examples/run_pe_nogenome.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# two paired-end ChIP samples with averaged replicates and no genome size
+
+# cpu and job parameters are machine dependent and set low for this example.
+
+# No genome size is specified, which in this case with the extremely sub-sampled 
+# bam files will result in poor q-value estimation and low or no peak calls in
+# the two samples.
+# This is primarily for testing purposes only!
+
+# environment build paths – not needed in production
+BLIB=${PWD}/../blib
+export PATH=${BLIB}/script:$PATH
+export PERL5LIB=${BLIB}/lib:$PERL5LIB
+
+# clean up previous run results
+rm -rf pe_nogenome
+
+echo
+echo "====================== paired-end ======================"
+echo
+
+multirep_macs2_pipeline.pl \
+--chip data/Rpd3_Ch1.bam,data/Rpd3_Ch2.bam,data/Rpd3_Ch3.bam \
+--control data/Rpd3_Input.bam \
+--name Rpd3 \
+--chip data/Tup1_Ch1.bam,data/Tup1_Ch2.bam,data/Tup1_Ch3.bam \
+--control data/Tup1_Input.bam \
+--name Tup1 \
+--pe \
+--dir pe_nogenome \
+--out pe_all \
+--dupfrac 0.05 \
+--optdist 2500 \
+--cutoff 3 \
+--peaksize 200 \
+--peakgap 100 \
+--cpu 1 \
+--job 4 
