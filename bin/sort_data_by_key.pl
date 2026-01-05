ddf6315f1dba6e8862e01747f8afbe0d40850cfc
Timothy Parnell <parnell.tj@gmail.com>
2026-01-05 16:57:37 -0700

    major rewrite to improve memory usage
    Re-worked the architecture to load files via Stream and not keep multiple object copies in memory. This can dramatically decrease memory usage for very large input files.

--- bin/sort_data_by_key.pl
+++ bin/sort_data_by_key.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl
 
 # documentation at end of file
 
@@ -9,10 +9,10 @@ use Getopt::Long qw(:config no_ignore_case bundling);
 use Pod::Usage;
 use List::Util qw(sum0);
 use Scalar::Util qw(looks_like_number);
-use Bio::ToolBox::Data;
+use Bio::ToolBox 2.03;
 use Bio::ToolBox::utility qw(parse_list);
 
-our $VERSION = 0.2;
+our $VERSION = 0.3;
 
 my $docs = <<DOC;
 
@@ -48,19 +48,19 @@ VERSION: $VERSION
 USAGE: sort_data_by_key.pl  -i <dataset_file>  -k <matrix_file> 
 
 REQUIRED:
-  -i --input <file>      The input data file
-  -k --key <file>        The matrix file of keys and groups
+  -i --input <file>       The input data file
+  -k --key <file>         The matrix file of keys and groups
 
-OPTIONS:
-  -g --group <index>     The group column(s) in the key file
-                           default is all columns except the first ID column
-  -v --values <index>    Specific data column indexes to sort by
-                           default is all identifiable data columns
-  -n --norm <index>      Specify the reference data columns for normalization
-  -f --format <int>      Specify the decimal positions when normalizing
-  -s --sort [count|name] Specify how to sort the groups: by decreasing 
-                           count or by name. Default is count.
-  -h --help              Show this help                           
+OPTIONS: 
+  -g --group <index>      The group column(s) in the key file
+                            default is all columns except the first ID column
+  -v --values <index>     Specific data column indexes to sort by
+                            default is all identifiable data columns
+  -n --norm <index>       Specify the reference data columns for normalization
+  -f --format <int>       Specify the decimal positions when normalizing
+  -s --sort [count|name]  Specify how to sort the groups: by decreasing 
+                            count or by name. Default is count.
+  -h --help               Show this help                           
 
 DOC
 
@@ -133,7 +133,7 @@ if ($valuelist) {
 
 # Load matrix
 print " Loading matrix $matrix_file...\n";
-my $Matrix = Bio::ToolBox::Data->new( file => $matrix_file )
+my $Matrix = Bio::ToolBox->load_file( $matrix_file )
 	or die " unable to open matrix file $matrix_file!\n";
 unless ( $Matrix->number_columns >= 2 ) {
 	die " key matrix must have at least 2 colunns!";
@@ -174,35 +174,35 @@ printf "  Loaded %d features for %d groups\n", scalar(keys %name2group),
 	scalar(keys %group2count);
 
 
-# Load data file
-print " Loading data $data_file...\n";
-my $Data = Bio::ToolBox::Data->new( file => $data_file )
+# Process the input data as a stream
+print " Parsing data $data_file...\n";
+my $InputStream = Bio::ToolBox->load_file( file => $data_file, stream => 1 )
 	or die " unable to open data file $data_file!\n";
-my $key_i = $Data->find_column( $Matrix->name(1) );
+my $key_i = $InputStream->find_column( $Matrix->name(1) );
 unless ($key_i) {
 	die sprintf(" unable to find key index column '%s' in data file '%s'!", 
 		$Matrix->name(1), $data_file);
 }
-$Data->iterate( sub {
-	my $row = shift;
+while ( my $row = $InputStream->next_row ) {
 	my $name = $row->value($key_i);
 	unless (exists $name2group{$name}) {
 		print " Feature '$name' not in matrix list!\n";
-		return;
+		next;
 	}
 
 	# get category
 	my $category = $name2group{$name};
 	unless ( exists $group2data{$category} ) {
-		$group2data{$category} = $Data->duplicate;
+		# generate a new Bio::ToolBox::Data object for this category
+		$group2data{$category} = $InputStream->duplicate;
 	}
 	$group2data{$category}->add_row($row);
-} );
+}
 
 # Identify data columns
 unless (@val_indexes) {
-	for my $i ( 1 .. $Data->last_column ) {
-		if ( $Data->metadata($i, 'dataset') ) {
+	for my $i ( 1 .. $InputStream->last_column ) {
+		if ( $InputStream->metadata($i, 'dataset') ) {
 			push @val_indexes, $i;
 		}
 	}
@@ -267,69 +267,78 @@ elsif ($sort eq 'name') {
 	@order = sort {$a cmp $b} keys %group2data;
 }
 
-# Concatenate the sorted category data into output data table
-my $OutData = $Data->duplicate;
-$OutData->add_comment("Sorted based on matrix key $matrix_file");
+# Prepare sorted output data table as a stream
+my $out_data_file = sprintf "%s%s.sorted%s", $InputStream->path, $InputStream->basename,
+		$InputStream->extension;
+my $OutStream = $InputStream->duplicate($out_data_file);
+$OutStream->add_comment("Sorted based on matrix key $matrix_file");
 if ($normlist) {
-	$OutData->add_comment( "Normalized values to reference columns $normlist" );
-}
-foreach my $group (@order) {
-	$group2data{$group}->iterate( sub {
-		my $row = shift;
-		$OutData->add_row($row);
-	} );
+	$OutStream->add_comment( "Normalized values to reference columns $normlist" );
 }
 
-# Write the merged final output data
-my $out_data_file = sprintf "%s%s.sorted%s", $Data->path, $Data->basename,
-		$Data->extension;
-my $success = $OutData->write_file($out_data_file);
-if ($success) {
-	print " Wrote new data file $success\n";
-}
-
-# Write individual summary files for each category
-foreach my $category ( keys %group2data ) {
-	my $sum_file = $group2data{$category}->summary_file(
-		filename => sprintf("%s%s.sorted_%s", $Data->path, $Data->basename, $category),
-		method   => 'trimmean'
-	);
-	if ($sum_file) {
-		print " Wrote summary file $sum_file\n";
-	}
-}
-
-# Write row key file
-my $OutMatrix = Bio::ToolBox::Data->new( columns => [ $Matrix->list_columns ] );
-# we need to translate the Name column from the matrix file to the ID column of the 
-# data file to be used as row identifiers in the plot_peak_figures.R plotting script
-# this is only pertinent in context of the peak calling pipeline, not necessarily 
-# other situations
-# use crude method to determine if a switch needs to be made
-my $switch;
-if ( $Matrix->name(1) eq 'Name' and $Data->name(1) eq 'Primary_ID' ) {
+# Prepare output matrix of the final sorted keys
+my $OutMatrix = Bio::ToolBox->new_data( $Matrix->list_columns );
+	# we need to translate the Name column from the matrix file to the ID column of the 
+	# data file to be used as row identifiers in the plot_peak_figures.R plotting script
+	# this is only pertinent in context of the peak calling pipeline, not necessarily 
+	# other situations
+	# use crude method to determine if a switch needs to be made
+my $switch = 0;
+if ( $Matrix->name(1) eq 'Name' and $InputStream->name(1) eq 'Primary_ID' ) {
 	$switch = 1;
 	$OutMatrix->name(1, 'Primary_ID');
 }
-$OutData->iterate( sub {
-	my $row = shift;
-	my $n   = $row->value($key_i);
-	my $i   = $name2row{$n};
-	if ($switch) {
-		my @val = $Matrix->get_row($i)->row_values();
-		shift @val; # remove existing name
-		$OutMatrix->add_row( [ $row->id, @val ] ); 
+
+
+# Go through each group and send appropriately to each output
+foreach my $group (@order) {
+
+	# work through each data row from each group data table
+	$group2data{$group}->iterate( sub {
+		my $row = shift;
+
+		# write to final merged output file stream
+		$OutStream->write_row($row);
+
+		# then add the original matrix data to the new matrix output
+		my $n = $row->value($key_i);
+		my $i = $name2row{$n};
+		if ($switch) {
+			my @val = $Matrix->get_row($i)->row_values();
+			shift @val; # remove existing name
+			$OutMatrix->add_row( [ $row->id, @val ] ); 
+		}
+		else {
+			$OutMatrix->add_row( $Matrix->get_row($i) );
+		}
+	} );
+
+	# then write a new summary file for this group
+	my $sum_file = sprintf("%s%s.sorted_%s", $InputStream->path, $InputStream->basename,
+		$group);
+	my $sum_file2 = $group2data{$group}->summary_file(
+		filename => $sum_file,
+		method   => 'trimmean'
+	);
+	if ($sum_file2) {
+		print " Wrote summary file $sum_file2\n";
 	}
-	else {
-		$OutMatrix->add_row( $Matrix->get_row($i) );
-	}
-} );
-my $out_rows_file = sprintf "%s%s.sorted.row_groups.txt", $Data->path, 
-		$Data->basename;
-$success = $OutMatrix->write_file($out_rows_file);
+}
+$OutStream->close_fh;
+printf " Wrote sorted data file %s\n", $out_data_file;
+
+
+# Write the final row key matrix for the sorted data table
+my $out_rows_file = sprintf "%s%s.sorted.row_groups.txt", $InputStream->path, 
+		$InputStream->basename;
+my $success = $OutMatrix->write_file($out_rows_file);
 if ($success) {
 	print " Wrote new row matrix annotation file $success\n";
 }
+
+
+# Finished
+exit 0;
 
 
 sub mean {
